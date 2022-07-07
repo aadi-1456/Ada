a021ba8da70844dbf304ce0da1887f4392e5f6bc
Aditya Lal <79254817+aadi-1456@users.noreply.github.com>
2022-07-07 22:47:27 +0530

    Update Selectionsort.cpp

--- Selectionsort.cpp
+++ Selectionsort.cpp
@@ -2,15 +2,13 @@
 using namespace std;
 
 void selectionsort(int *a,int n){
-    int i,j,min,temp;
-    for(i=0;i<n-1;i++){                  //                              64, 25, 12, 22, 11
-        min=i;                           //   time complexity        First pass:  64   	   25   	   12   	   22   	   11   
-        j=i+1;                           //  best case    :- n^2                  11   	   25   	   12   	   22   	   64  
-        while(j<n){                      //  average case :- n^2     Second Pass: 11   	   25   	   12   	   22   	   64   
-            if(a[min]>a[j]){             //  worst case   :- n^2                  11   	   12   	   25   	   22   	   64   
+    int temp;
+    for(i=0;i<n-1;i++){
+        int min=a[i];
+        for(j=i+1;j<n;j++){
+            if(a[i]>a[j]){
                 min=j;
             }
-            j++;
         }
         temp=a[min];
         a[min]=a[i];
